ad424acc1ee87cc9bc120a5c1a424632f95b4148
Elong-Steven <elong_steve@126.com>
2021-09-26 16:42:20 +0800

    能耗优化

--- Public/api.js
+++ Public/api.js
@@ -6,128 +6,128 @@ for (var i = 1; i <= 88; i++) {
 }
 
 // 创建KEYMAP API
-window.pianist = function (M, outTime) {
+var KeyMap = new Array(); // Use The SquareArray To Create The KeyBoard
+KeyMap["C"] = new Array();
+KeyMap["#C"] = new Array();
+KeyMap["D"] = new Array();
+KeyMap["#D"] = new Array();
+KeyMap["E"] = new Array();
+KeyMap["F"] = new Array();
+KeyMap["#F"] = new Array();
+KeyMap["G"] = new Array();
+KeyMap["#G"] = new Array();
+KeyMap["A"] = new Array();
+KeyMap["#A"] = new Array();
+KeyMap["B"] = new Array();
+//大字二组 0
+KeyMap["A"][0] = 1;
+KeyMap["#A"][0] = 2;
+KeyMap["B"][0] = 3;
+//大字一组 1
+KeyMap["C"][1] = 4;
+KeyMap["#C"][1] = 5;
+KeyMap["D"][1] = 6;
+KeyMap["#D"][1] = 7;
+KeyMap["E"][1] = 8;
+KeyMap["F"][1] = 9;
+KeyMap["#F"][1] = 10;
+KeyMap["G"][1] = 11;
+KeyMap["#G"][1] = 12;
+KeyMap["A"][1] = 13;
+KeyMap["#A"][1] = 14;
+KeyMap["B"][1] = 15;
+//大字组 2
+KeyMap["C"][2] = 16;
+KeyMap["#C"][2] = 17;
+KeyMap["D"][2] = 18;
+KeyMap["#D"][2] = 19;
+KeyMap["E"][2] = 20;
+KeyMap["F"][2] = 21;
+KeyMap["#F"][2] = 22;
+KeyMap["G"][2] = 23;
+KeyMap["#G"][2] = 24;
+KeyMap["A"][2] = 25;
+KeyMap["#A"][2] = 26;
+KeyMap["B"][2] = 27;
+//小字组 3
+KeyMap["C"][3] = 28;
+KeyMap["#C"][3] = 29;
+KeyMap["D"][3] = 30;
+KeyMap["#D"][3] = 31;
+KeyMap["E"][3] = 32;
+KeyMap["F"][3] = 33;
+KeyMap["#F"][3] = 34;
+KeyMap["G"][3] = 35;
+KeyMap["#G"][3] = 36;
+KeyMap["A"][3] = 37;
+KeyMap["#A"][3] = 38;
+KeyMap["B"][3] = 39;
+//小字一组 4
+KeyMap["C"][4] = 40;
+KeyMap["#C"][4] = 41;
+KeyMap["D"][4] = 42;
+KeyMap["#D"][4] = 43;
+KeyMap["E"][4] = 44;
+KeyMap["F"][4] = 45;
+KeyMap["#F"][4] = 46;
+KeyMap["G"][4] = 47;
+KeyMap["#G"][4] = 48;
+KeyMap["A"][4] = 49;
+KeyMap["#A"][4] = 50;
+KeyMap["B"][4] = 51;
+//小字二组 5
+KeyMap["C"][5] = 52;
+KeyMap["#C"][5] = 53;
+KeyMap["D"][5] = 54;
+KeyMap["#D"][5] = 55;
+KeyMap["E"][5] = 56;
+KeyMap["F"][5] = 57;
+KeyMap["#F"][5] = 58;
+KeyMap["G"][5] = 59;
+KeyMap["#G"][5] = 60;
+KeyMap["A"][5] = 61;
+KeyMap["#A"][5] = 62;
+KeyMap["B"][5] = 63;
+//小字三组 6
+KeyMap["C"][6] = 64;
+KeyMap["#C"][6] = 65;
+KeyMap["D"][6] = 66;
+KeyMap["#D"][6] = 67;
+KeyMap["E"][6] = 68;
+KeyMap["F"][6] = 69;
+KeyMap["#F"][6] = 70;
+KeyMap["G"][6] = 71;
+KeyMap["#G"][6] = 72;
+KeyMap["A"][6] = 73;
+KeyMap["#A"][6] = 74;
+KeyMap["B"][6] = 75;
+//小字四组 7
+KeyMap["C"][7] = 76;
+KeyMap["#C"][7] = 77;
+KeyMap["D"][7] = 78;
+KeyMap["#D"][7] = 79;
+KeyMap["E"][7] = 80;
+KeyMap["F"][7] = 81;
+KeyMap["#F"][7] = 82;
+KeyMap["G"][7] = 83;
+KeyMap["#G"][7] = 84;
+KeyMap["A"][7] = 85;
+KeyMap["#A"][7] = 86;
+KeyMap["B"][7] = 87;
+//小字五组 8
+KeyMap["C"][8] = 88;
+
+//定义降号小字组
+KeyMap["bD"] = KeyMap["#C"];
+KeyMap["bE"] = KeyMap["#D"];
+KeyMap["bG"] = KeyMap["#F"];
+KeyMap["bA"] = KeyMap["#G"];
+KeyMap["bB"] = KeyMap["#A"];
+
+window.pianist = function(M, outTime) {
   stop();
   if (!outTime) outTime = 0;
   var speed = M.speed; // It's based on the quarter note, The speed of playing per minute.
-  // Use The SquareArray To Create The KeyBoard
-  var KeyMap = new Array();
-  KeyMap["C"] = new Array();
-  KeyMap["#C"] = new Array();
-  KeyMap["D"] = new Array();
-  KeyMap["#D"] = new Array();
-  KeyMap["E"] = new Array();
-  KeyMap["F"] = new Array();
-  KeyMap["#F"] = new Array();
-  KeyMap["G"] = new Array();
-  KeyMap["#G"] = new Array();
-  KeyMap["A"] = new Array();
-  KeyMap["#A"] = new Array();
-  KeyMap["B"] = new Array();
-  //大字二组 0
-  KeyMap["A"][0] = 1;
-  KeyMap["#A"][0] = 2;
-  KeyMap["B"][0] = 3;
-  //大字一组 1
-  KeyMap["C"][1] = 4;
-  KeyMap["#C"][1] = 5;
-  KeyMap["D"][1] = 6;
-  KeyMap["#D"][1] = 7;
-  KeyMap["E"][1] = 8;
-  KeyMap["F"][1] = 9;
-  KeyMap["#F"][1] = 10;
-  KeyMap["G"][1] = 11;
-  KeyMap["#G"][1] = 12;
-  KeyMap["A"][1] = 13;
-  KeyMap["#A"][1] = 14;
-  KeyMap["B"][1] = 15;
-  //大字组 2
-  KeyMap["C"][2] = 16;
-  KeyMap["#C"][2] = 17;
-  KeyMap["D"][2] = 18;
-  KeyMap["#D"][2] = 19;
-  KeyMap["E"][2] = 20;
-  KeyMap["F"][2] = 21;
-  KeyMap["#F"][2] = 22;
-  KeyMap["G"][2] = 23;
-  KeyMap["#G"][2] = 24;
-  KeyMap["A"][2] = 25;
-  KeyMap["#A"][2] = 26;
-  KeyMap["B"][2] = 27;
-  //小字组 3
-  KeyMap["C"][3] = 28;
-  KeyMap["#C"][3] = 29;
-  KeyMap["D"][3] = 30;
-  KeyMap["#D"][3] = 31;
-  KeyMap["E"][3] = 32;
-  KeyMap["F"][3] = 33;
-  KeyMap["#F"][3] = 34;
-  KeyMap["G"][3] = 35;
-  KeyMap["#G"][3] = 36;
-  KeyMap["A"][3] = 37;
-  KeyMap["#A"][3] = 38;
-  KeyMap["B"][3] = 39;
-  //小字一组 4
-  KeyMap["C"][4] = 40;
-  KeyMap["#C"][4] = 41;
-  KeyMap["D"][4] = 42;
-  KeyMap["#D"][4] = 43;
-  KeyMap["E"][4] = 44;
-  KeyMap["F"][4] = 45;
-  KeyMap["#F"][4] = 46;
-  KeyMap["G"][4] = 47;
-  KeyMap["#G"][4] = 48;
-  KeyMap["A"][4] = 49;
-  KeyMap["#A"][4] = 50;
-  KeyMap["B"][4] = 51;
-  //小字二组 5
-  KeyMap["C"][5] = 52;
-  KeyMap["#C"][5] = 53;
-  KeyMap["D"][5] = 54;
-  KeyMap["#D"][5] = 55;
-  KeyMap["E"][5] = 56;
-  KeyMap["F"][5] = 57;
-  KeyMap["#F"][5] = 58;
-  KeyMap["G"][5] = 59;
-  KeyMap["#G"][5] = 60;
-  KeyMap["A"][5] = 61;
-  KeyMap["#A"][5] = 62;
-  KeyMap["B"][5] = 63;
-  //小字三组 6
-  KeyMap["C"][6] = 64;
-  KeyMap["#C"][6] = 65;
-  KeyMap["D"][6] = 66;
-  KeyMap["#D"][6] = 67;
-  KeyMap["E"][6] = 68;
-  KeyMap["F"][6] = 69;
-  KeyMap["#F"][6] = 70;
-  KeyMap["G"][6] = 71;
-  KeyMap["#G"][6] = 72;
-  KeyMap["A"][6] = 73;
-  KeyMap["#A"][6] = 74;
-  KeyMap["B"][6] = 75;
-  //小字四组 7
-  KeyMap["C"][7] = 76;
-  KeyMap["#C"][7] = 77;
-  KeyMap["D"][7] = 78;
-  KeyMap["#D"][7] = 79;
-  KeyMap["E"][7] = 80;
-  KeyMap["F"][7] = 81;
-  KeyMap["#F"][7] = 82;
-  KeyMap["G"][7] = 83;
-  KeyMap["#G"][7] = 84;
-  KeyMap["A"][7] = 85;
-  KeyMap["#A"][7] = 86;
-  KeyMap["B"][7] = 87;
-  //小字五组 8
-  KeyMap["C"][8] = 88;
-
-  //定义降号小字组
-  KeyMap["bD"] = KeyMap["#C"];
-  KeyMap["bE"] = KeyMap["#D"];
-  KeyMap["bG"] = KeyMap["#F"];
-  KeyMap["bA"] = KeyMap["#G"];
-  KeyMap["bB"] = KeyMap["#A"];
 
   // Program Of Music Build 打谱程序
   var i = 0;
